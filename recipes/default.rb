65699bddad9b7bebfc37bd238b5ab124265b43eb
Guilhem Lettron <guilhem@lettron.fr>
2012-10-11 11:58:45 +0200

    buggy reload path isn't used anymore

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,21 +25,3 @@ powershell "install chocolatey" do
   notifies :create, "ruby_block[reload path]", :immediately
   not_if { ::File.exist?( ::File.join(node['chocolatey']['bin_path'], "chocolatey.bat") ) }
 end
-
-ruby_block "reload path" do
-  block do
-    require 'Win32API'
-    #see: http://msdn.microsoft.com/en-us/library/ms682653%28VS.85%29.aspx
-    HWND_BROADCAST = 0xffff
-    WM_SETTINGCHANGE = 0x001A
-    SMTO_BLOCK = 0x0001
-    SMTO_ABORTIFHUNG = 0x0002
-    SMTO_NOTIMEOUTIFNOTHUNG = 0x0008
-    result = 0
-    flags = SMTO_BLOCK | SMTO_ABORTIFHUNG | SMTO_NOTIMEOUTIFNOTHUNG
-    @send_message ||= Win32API.new('user32', 'SendMessageTimeout', 'LLLPLLP', 'L')
-    @send_message.call(HWND_BROADCAST, WM_SETTINGCHANGE, 0, 'Environment', flags, 5000, result)
-  end
-  action :create
-  only_if { RUBY_PLATFORM =~ /mswin|mingw32|windows/ }
-end
