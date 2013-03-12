65de33935a9487cb157cf1e9bd08bbf1aa651340
Owain Perry <owain.perry@thetrainline.com>
2013-03-12 15:51:35 +0000

    Added an attribute for the chocolatey download Uri so it can be changed if necessary

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
-if node['platform_family'] == "windows"
-  default['chocolatey']['path'] = ::File.join( ENV['SYSTEMDRIVE'], "Chocolatey")
-  default['chocolatey']['bin_path'] = ::File.join( node['chocolatey']['path'], "bin")
-end
+default['chocolatey']['Uri'] = "https://raw.github.com/chocolatey/chocolatey/master/chocolateyInstall/InstallChocolatey.ps1"
+default['chocolatey']['path'] = ::File.join( ENV['SYSTEMDRIVE'], "Chocolatey")
+default['chocolatey']['bin_path'] = ::File.join( node['chocolatey']['path'], "bin")
