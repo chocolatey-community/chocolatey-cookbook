f85dc4697b136ce1c3a1cc533f9329542f44b84e
Owain Perry <owain.perry@thetrainline.com>
2013-03-17 09:54:21 +0000

    inc of the cookbook version number

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache 2.0"
 description      "Install chocolatey and packages on Windows"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 
 depends "powershell"
 supports "windows"
