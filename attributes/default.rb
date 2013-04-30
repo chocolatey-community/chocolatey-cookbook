68a751aff9faa11ec20e7f4e01a1ad14f2d46b2f
Guilhem Lettron <guilhem@lettron.fr>
2013-04-30 12:03:29 +0200

    fix depends on !windows node

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
-default['chocolatey']['path'] = ::File.join( ENV['SYSTEMDRIVE'], "Chocolatey")
-
-default['chocolatey']['bin_path'] = ::File.join( node['chocolatey']['path'], "bin")
+if node['platform_family'] == "windows"
+  default['chocolatey']['path'] = ::File.join( ENV['SYSTEMDRIVE'], "Chocolatey")
+  default['chocolatey']['bin_path'] = ::File.join( node['chocolatey']['path'], "bin")
+end
