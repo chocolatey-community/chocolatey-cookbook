a78a1058d1ec981a6bd2ff4f0cff735edfdf1ff7
andrewmyhre <andrew@justgiving.com>
2014-08-27 15:14:00 +0100

    adds support for params, x86 and force arguments

--- resources/default.rb
+++ resources/default.rb
@@ -4,6 +4,9 @@ attribute :package, :kind_of => String, :name_attribute => true
 attribute :source, :kind_of => String
 attribute :version, :kind_of => String
 attribute :args, :kind_of => String
+attribute :choco_params, :kind_of => String, :default=>nil
+attribute :force, :kind_of => [TrueClass, FalseClass], :default=>false
+attribute :x86, :kind_of => [TrueClass, FalseClass], :default=>false
 
 def initialize(*args)
   super
