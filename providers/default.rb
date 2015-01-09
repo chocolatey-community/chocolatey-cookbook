9316d8e95c266fe73f2e642933ae49b2ff71004c
Jay Mundrawala <jdmundrawala@gmail.com>
2015-01-09 11:33:14 -0800

    The package name should be case insensitive

--- providers/default.rb
+++ providers/default.rb
@@ -92,11 +92,11 @@ def package_exists?(name, version)
   cmd.run_command
   software = cmd.stdout.split("\r\n").each_with_object({}) do |s, h|
     v, k = s.split
-    h[String(v).strip] = String(k).strip
+    h[String(v).strip.downcase] = String(k).strip
     h
   end
 
-  software[name] == version
+  software[name.downcase] == version
 end
 
 def upgradeable?(name)
