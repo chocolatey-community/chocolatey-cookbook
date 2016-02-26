5669314fb44fa649212212dd5b16291950f0ccec
Michael Gaida <mgaida@goodgamestudios.com>
2016-02-26 14:13:14 +0100

    moved env_path into the public scope, cause its used in the provider

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -30,6 +30,15 @@ module Chocolatey
       @is_chocolatey_installed = (cmd.exitstatus == 0)
     end
 
+    # combine the local path with the user and machine paths
+    def env_path(local_path)
+      machine  = env_var('PATH', 'MACHINE').split(';')
+      user     = env_var('PATH', 'USER').split(';')
+      local    = local_path.split(';')
+      combined = local.concat(machine).concat(user).uniq.compact
+      combined.join(';')
+    end
+
     private
 
     def machine_env_var(env_var)
@@ -41,15 +50,6 @@ module Chocolatey
         "[System.Environment]::GetEnvironmentVariable('#{env_var}', '#{scope}')"
       )
       env_var.stdout.chomp
-    end
-
-    # combine the local path with the user and machine paths
-    def env_path(local_path)
-      machine  = env_var('PATH', 'MACHINE').split(';')
-      user     = env_var('PATH', 'USER').split(';')
-      local    = local_path.split(';')
-      combined = local.concat(machine).concat(user).uniq.compact
-      combined.join(';')
     end
   end
 end
