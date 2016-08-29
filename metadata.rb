0d871005a1ecc9c8cfc31f351ddec8eeefbe49c0
Matt Wrock <matt@mattwrock.com>
2016-08-29 01:57:02 -0700

    release 1.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem.lettron@youscribe.com'
 license          'Apache 2.0'
 description      'Install chocolatey and packages on Windows'
 long_description 'Installs the Chocolatey package manager for Windows and provides a Chef resource for installing nuget packages from https://chocolatey.org/'
-version          '1.0.1'
+version          '1.0.2'
 
 source_url 'https://github.com/chocolatey/chocolatey-cookbook' if defined?(:source_url)
 issues_url 'https://github.com/chocolatey/chocolatey-cookbook/issues' if defined?(:issues_url)
