9df29cfa6a5d886c06180d2fd456f1d8e989321c
Tim Smith <tsmith@chef.io>
2017-04-13 10:47:27 -0700

    Use the SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'chocolatey'
 maintainer       'Guilhem Lettron'
 maintainer_email 'guilhem.lettron@youscribe.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Install chocolatey and packages on Windows'
 long_description 'Installs the Chocolatey package manager for Windows and provides a Chef resource for installing nuget packages from https://chocolatey.org/'
 version          '1.1.0'
