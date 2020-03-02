85cf51f9b82607b6296561b67f1f2ae006e3c10e
Joost van der Hoff <joost.van.der.hoff@exact.com>
2020-03-02 16:35:38 +0100

    Upgrade Chocolatey using chocolatey_package if upgrade requested

--- recipes/default.rb
+++ recipes/default.rb
@@ -38,3 +38,9 @@ powershell_script 'Install Chocolatey' do
   code install_ps1
   not_if { chocolatey_installed? }
 end
+
+chocolatey_package 'chocolatey' do
+  action :upgrade
+  version node['chocolatey']['install_vars']['chocolateyVersion']
+  only_if { node['chocolatey']['upgrade'] }
+end
